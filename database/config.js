fc5911dc0836a471837f2b49d14ef1588988fb8b
Pempi <asdd85738@gmail.com>
2025-09-19 20:40:12 -0600

    filess.io

--- database/config.js
+++ database/config.js
@@ -7,7 +7,9 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        dialect: 'mariadb'
+        port: process.env.DB_PORT,
+        dialect: 'mariadb',
+        dialectOptions: { connectTimeout: 10000 }
     }
 );
 
